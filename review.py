9dfaea228e1ab63dfa6c01366912cadff3a1c5ec
Noah <106498918+swifty-hh@users.noreply.github.com>
2024-02-20 17:36:03 +0900

    Add files via upload

---
+++ review.py
@@ -0,0 +1,36 @@
+my_number = 5
+# my_number를 float 타입으로 변환하고, 출력해보세요.
+my_number =5.
+print(my_number)
+
+my_string = "Hello, World!"
+# my_string에서 첫 번째 'l'이 나오는 위치를 찾아보세요.
+print(my_string[1])
+
+my_list = [1, 2, 3, 4, 5]
+# my_list에 6을 추가하고, 출력해보세요.
+my_list.append(6)
+print(my_list)
+
+my_tuple = (1, 2, 3, 4, 5)
+# my_tuple의 길이를 출력해보세요.
+print(len(my_tuple))
+
+my_dict = {"name": "Python", "type": "Programming Language"}
+my_dict['year'] = 1991  # 'year': 1991을 추가합니다.
+my_dict['like'] = '99%'
+print(my_dict)
+
+my_set = {1, 2, 3, 4, 5}
+# my_set에 6을 추가하고, 출력해보세요.
+my_set.add(6)
+print(my_set)
+
+my_string = "Hello, Python!"
+# my_string을 모두 대문자로 변환하고, 출력해보세요.
+cap_str = my_string.upper
+print(cap_str)
+
+my_string = "Hello, Python, World!"
+# my_string을 ", "으로 분리하고, 결과를 출력해보세요.
+print(my_string.split(", "))
